200b3fe2894c454c10d0b58dfc8111cc3face112
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-04-08 17:06:33 +0300

    feat: build UI of Logo

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import './NavBar.styles.css';
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span className="logo__icon" role="img">
+        🍿
+      </span>
+      <h1 className="logo__title">usePopcorn</h1>
+    </div>
+  );
+}
