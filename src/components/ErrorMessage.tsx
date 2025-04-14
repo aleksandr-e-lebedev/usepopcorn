98209d53b80aace3b1ab834ccb62c7fd4058fa43
Aleksandr Lebedev <lebe.dev@outlook.com>
2025-04-14 17:13:01 +0300

    feat: build UI of ErrorMessage

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,13 @@
+import './ErrorMessage.styles.css';
+
+interface Props {
+  message: string;
+}
+
+export default function ErrorMessage({ message }: Props) {
+  return (
+    <p className="error">
+      <span>⛔</span> {message}
+    </p>
+  );
+}
